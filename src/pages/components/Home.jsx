f150ae90e3f3d0f01e121ce1466221818783bc27
Krishala Chaudhary <159783054+krishala123@users.noreply.github.com>
2025-07-20 20:37:09 +0545

    Add files via upload

---
+++ src/pages/components/Home.jsx
@@ -0,0 +1,43 @@
+import {useEffect,useState} from "react";
+import Navbar from "./components/NavBar";
+import Card from "./components/Card";
+import axios from "axios";
+import NavBar from "./components/NavBar";
+
+
+function Home(){
+  const[Blogs,setBlogs]=useState([]);
+  async function fetchBlogs(){
+    const response =await axios.get("https://687af394abb83744b7ee4905.mockapi.io/Blogs")
+    console.log(response)
+    if(response.status==200){
+      setBlogs(response.data)
+
+    }
+    else{
+      alert("Error aayoo!!!")
+    }
+  }
+  useEffect(()=>{
+    fetchBlogs();
+  },[])
+
+  console.log(Blogs,"This is blogs")
+  
+
+  return (
+    <>
+    <NavBar/>
+    <div className="flex  justify-between flex-wrap p-5 gap-4">
+        {Blogs.map(function(Blog){
+          return(
+            <Card key={Blog.id} Blog={Blog} />
+          )
+        })}
+    </div>
+</>
+
+  );
+}
+
+export default Home;
